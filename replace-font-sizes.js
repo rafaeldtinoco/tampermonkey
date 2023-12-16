03463c8a2bf687a0219ec369c5417f21b4948fbd
Rafael David Tinoco <rafaeldtinoco@gmail.com>
2023-12-16 02:33:34 -0300

    add font replacement scripts

---
+++ replace-font-sizes.js
@@ -0,0 +1,143 @@
+// ==UserScript==
+// @name         replace-font-sizes
+// @namespace    http://tampermonkey.net/
+// @version      202312160200
+// @description  Replace all fonts sizes by preferred ones.
+// @author       ChatGPT
+// @match        http*://*/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+; (function () {
+  'use strict'
+
+  var fixedWidthFonts = [
+    'Monospace',
+    'Courier',
+    'Courier New',
+    'Consolas',
+    'Monaco',
+    'Menlo',
+    'Fire Mono',
+    'Liberation Mono',
+    'Monospace',
+    'Noto Mono',
+    'Roboto Mono'
+  ]
+
+  var domainFontSettings = [
+    {
+      domain: 'github.com',
+      minFontSize: 17,
+      maxFontSize: 24,
+      minFixedWidthFontSize: 17,
+      maxFixedWidthFontSize: 17,
+      lineHeight: 1.2,
+      fixedWidthLineHeight: 1.2
+    },
+    {
+      domain: 'google.com',
+      minFontSize: 16,
+      maxFontSize: 22,
+      minFixedWidthFontSize: 16,
+      maxFixedWidthFontSize: 22,
+      lineHeight: 0,
+      fixedWidthLineHeight: 0
+    },
+    {
+      domain: 'openai.com',
+      minFontSize: 18,
+      maxFontSize: 18,
+      minFixedWidthFontSize: 18,
+      maxFixedWidthFontSize: 18,
+      lineHeight: 1.4,
+      fixedWidthLineHeight: 1.2
+    }
+  ]
+
+  function adjustFontSize(node) {
+    let currentDomain = window.location.hostname
+    let fontFamily = window.getComputedStyle(node).fontFamily.toLowerCase()
+    let fontSize = parseFloat(window.getComputedStyle(node).fontSize)
+    let isFixedWidth = fixedWidthFonts.some(font =>
+      fontFamily.includes(font.toLowerCase())
+    )
+
+    let applicableSettings = domainFontSettings.find(setting =>
+      currentDomain.includes(setting.domain)
+    )
+    if (!applicableSettings) return
+
+    let newFontSize = 0
+    let newLineHeight = 0
+
+    let {
+      minFontSize,
+      maxFontSize,
+      minFixedWidthFontSize,
+      maxFixedWidthFontSize,
+      lineHeight,
+      fixedWidthLineHeight
+    } = applicableSettings
+
+    if (isFixedWidth) {
+      if (fontSize < minFixedWidthFontSize) {
+        newFontSize = minFixedWidthFontSize
+      }
+      if (fontSize > maxFixedWidthFontSize) {
+        newFontSize = maxFixedWidthFontSize
+      }
+      if (fixedWidthLineHeight) {
+        newLineHeight = fixedWidthLineHeight
+      }
+    } else {
+      if (fontSize < minFontSize) {
+        newFontSize = minFontSize
+      }
+      if (fontSize > maxFontSize) {
+        newFontSize = maxFontSize
+      }
+      if (lineHeight) {
+        newLineHeight = lineHeight
+      }
+    }
+
+    if (newFontSize) {
+      node.style.fontSize = `${newFontSize}px`
+    }
+    if (newLineHeight) {
+      node.style.lineHeight = `${newLineHeight}em`
+    }
+  }
+
+  function observeDOMChanges() {
+    const observer = new MutationObserver(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          if (node.nodeType === Node.ELEMENT_NODE) {
+            adjustFontSize(node)
+            node.querySelectorAll('*').forEach(adjustFontSize)
+          }
+        })
+        if (mutation.type === 'attributes') {
+          adjustFontSize(mutation.target)
+        }
+      })
+    })
+    observer.observe(document, {
+      childList: true,
+      subtree: true,
+      attributes: true
+    })
+  }
+
+  window.addEventListener('load', () => {
+    document.querySelectorAll('*').forEach(adjustFontSize)
+    observeDOMChanges()
+  })
+
+  document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('*').forEach(adjustFontSize)
+  })
+})()
---
+++ replace-fonts.js
@@ -0,0 +1,103 @@
+// ==UserScript==
+// @name         replace-fonts
+// @namespace    http://tampermonkey.net/
+// @version      202312160200
+// @description  Replace all fonts by preferred ones.
+// @author       Rafael David Tinoco
+// @match        http*://*/*
+// @run-at       document-idle
+// @grant        none
+// ==/UserScript==
+
+; (function () {
+    'use strict'
+
+    var fontsToOpenSans = [
+        'Calibri',
+        'Cambria',
+        'Candara',
+        'Constantia',
+        'Corbel',
+        'Georgia',
+        'Segoe UI',
+        'Trebuchet MS',
+        'Verdana',
+        'sans',
+        'sans-serif',
+        'serif',
+        'Avenir',
+        'Avenir Next',
+        'Comic Sans MS',
+        'Comic Sans',
+        'Lucida Grande',
+        'Lucida Sans',
+        'Roboto'
+    ]
+    var fontsToCousine = [
+        'Monospace',
+        'Noto Mono',
+        'Consolas',
+        'Courier New',
+        'Courier',
+        'Monaco',
+        'Menlo',
+        'Fira Mono',
+        'Liberation Mono',
+        'Roboto Mono'
+    ]
+    var fontsToLiberationSans = [
+        'Arial',
+        'Times New Roman',
+        'Arial Narrow',
+        'Tahoma',
+        'Helvetica',
+        'Helvetica Neue',
+        'San Francisco',
+        'Fira Sans',
+        'Noto Sans'
+    ]
+
+    function adjustElementStyles(node) {
+        let fontFamily = window
+            .getComputedStyle(node)
+            .fontFamily.replace(/["']/g, '')
+        let site = window.location.hostname
+
+        fontFamily.split(',').some(font => {
+            if (fontsToOpenSans.includes(font.trim())) {
+                node.style.fontFamily = '"Open Sans", sans-serif'
+                return true
+            } else if (fontsToCousine.includes(font.trim())) {
+                node.style.fontFamily = 'Cousine, monospace'
+                return true
+            } else if (fontsToLiberationSans.includes(font.trim())) {
+                node.style.fontFamily = '"Liberation Sans", sans-serif'
+                return true
+            }
+            return false
+        })
+    }
+
+    function observeDOMChanges() {
+        const observer = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                mutation.addedNodes.forEach(node => {
+                    if (node.nodeType === Node.ELEMENT_NODE) {
+                        adjustElementStyles(node)
+                        node.querySelectorAll('*').forEach(adjustElementStyles)
+                    }
+                })
+            })
+        })
+        observer.observe(document, { childList: true, subtree: true })
+    }
+
+    window.addEventListener('load', () => {
+        document.querySelectorAll('*').forEach(adjustElementStyles)
+        observeDOMChanges()
+    })
+
+    document.addEventListener('DOMContentLoaded', () => {
+        document.querySelectorAll('*').forEach(adjustElementStyles)
+    })
+})()
